90f357a1e4d51e856c670b5dd1420b4c72257c99
salinger2333 <1565664814@qq.com>
2023-04-26 18:36:50 +0800

    开发至layout,登录已完成

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-befroe-function-paren': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'warn',
+    'space-comment': 'off',
+    'no-useless-return': 'off',
+    'no-trailing-spaces': 'off',
+    'space-before-function-paren': 'off'
+  }
+}
